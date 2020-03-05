f71f2ebf9e7f5737d3a5836cf6136d81ee0106e3
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-05 11:33:20 +0100

    :recycle: Upgrade HTTP for support of HTTP v2

--- src/GraphQl/Http.elm
+++ src/GraphQl/Http.elm
@@ -1,7 +1,8 @@
-module GraphQl.Http exposing (send)
+module GraphQl.Http exposing (Options, send)
 
 {-| This module provides helpers to directly deal with HTTP with GraphQL.
 
+@docs Options
 @docs send
 -}
 
@@ -9,16 +10,34 @@ import GraphQl
 import Json.Decode as Decode exposing (Decoder)
 import Http exposing (Error)
 
+{-| Defines all the options for a GraphQl request. -}
+type alias Options =
+  { url : String
+  , headers : List Http.Header
+  }
+
 {-| Send a GraphQL request directly through an HTTP post, directly decoding the
 `data` field in the response.
+
+    graphQlRequestOptions : GraphQl.Options
+    graphQlRequestOptions =
+      { url = "/example_endpoint"
+      , headers = []
+      }
 
     sendMyAwesomeRequest : (Result Http.Error a -> msg) -> Decoder a -> Cmd msg
     sendMyAwesomeRequest msg decoder =
       GraphQl.query myAwesomeRequest
-        |> GraphQl.Http.send "/example_endpoint" msg decoder
+      |> GraphQl.Http.send graphQlRequestOptions msg decoder
 -}
-send : String -> (Result Error c -> msg) -> Decoder c -> GraphQl.Request a b -> Cmd msg
-send endpoint msg decoder body =
-   (Decode.field "data" decoder)
-    |> Http.post endpoint (Http.jsonBody (GraphQl.toJson body))
-    |> Http.send msg
+send : Options -> (Result Error c -> msg) -> Decoder c -> GraphQl.Request a b -> Cmd msg
+send { url, headers } msg decoder body =
+  { method = "POST"
+  , headers = headers
+  , url = url
+  , body = Http.jsonBody (GraphQl.toJson body)
+  , timeout = Nothing
+  , tracker = Nothing
+  , expect = Http.expectJson msg (Decode.field "data" decoder)
+  }
+  |> Http.request
